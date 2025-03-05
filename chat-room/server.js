f9fe71cbfa5ab7d517f69363b1d9925e2fc30ec8
Timco307 <124943543+Timco307@users.noreply.github.com>
2025-03-05 03:16:15 +0000

    new and good?????

---
+++ chat-room/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('joinRoom', ({ username, room }) => {
+    socket.join(room);
+    socket.broadcast.to(room).emit('message', `${username} has joined the room`);
+  });
+
+  socket.on('chatMessage', (msg) => {
+    const { room, username, message } = msg;
+    io.to(room).emit('message', `${username}: ${message}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
